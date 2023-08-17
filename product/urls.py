d41d114ef0a9bbaa31775adbc6b17a2ff73d0639
ran <ran@goz.com>
2023-08-17 17:37:47 +0300

    product Create added

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('product', views.products, name='products'),
+    path('product/create', views.create_product,name='create_product'),
     path('category', views.categories,name='categories'),
 ]
--- product/views.py
+++ product/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from .models import Product, Category
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 
 @api_view()
@@ -17,6 +18,16 @@ def products(request):
         all = all.filter(name__contains=search_str)
     all_products = ProductSerializer(all,many=True).data
     return Response(all_products)
+
+@csrf_exempt
+@api_view(["POST"])
+def create_product(request):
+    data = JSONParser().parse(request)
+    serializer = ProductSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 
 @api_view()
