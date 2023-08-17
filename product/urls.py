7e4cf3f73957c2d53e0dd63a3e0c74e1741988e6
ran <ran@goz.com>
2023-08-17 16:53:03 +0300

    removed category implementation

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('product', views.products, name='products'),
-    path('category', views.categories,name='categories'),
 ]
