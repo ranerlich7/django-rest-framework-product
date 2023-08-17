575beab0d4acf0ae8e3a152706141da0840af77a
ran <ran@goz.com>
2023-08-17 16:33:41 +0300

    category added

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
-from .models import MyUser, Product
+from .models import MyUser, Product, Category
 
 admin.site.register(MyUser, UserAdmin)
 admin.site.register(Product)
+admin.site.register(Category)
--- product/views.py
+++ product/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 # from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.decorators import api_view
 
 
@@ -13,3 +13,10 @@ from rest_framework.decorators import api_view
 def products(request):
     all_products = ProductSerializer(Product.objects.all(), many=True).data
     return Response(all_products)
+
+
+@api_view()
+def categories(request):
+    all_categories = CategorySerializer(Category.objects.all(), many=True).data
+    return Response(all_categories)
+
