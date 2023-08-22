d45637deb3a57d58f8fef01dfb9346b3db8140fa
ran <ran@goz.com>
2023-08-22 12:29:14 +0300

    added product detail CRUD

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('product', views.products, name='products'),
+    path('product/<pk>', views.product_detail, name='product_detail'),
     path('category', views.categories,name='categories'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from .models import Product, Category
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.response import Response
+from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
@@ -33,3 +34,24 @@ def categories(request):
     all_categories = CategorySerializer(Category.objects.all(), many=True).data
     return Response(all_categories)
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
