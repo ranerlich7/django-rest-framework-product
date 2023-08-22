d3b8cd9df8be466b44c1acadffdf4ba021c05e1a
kobi <kobi.shasha@gmail.com>
2023-08-22 21:22:57 +0300

    added CORS to enable call from live server

--- product_proj/settings.py
+++ product_proj/settings.py
@@ -39,12 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5501",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,5 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-  
