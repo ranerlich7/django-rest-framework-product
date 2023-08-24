ce73cd499749772ae769f69e19b52d34f24e29cd
ran <ran@goz.com>
2023-08-24 14:51:37 +0300

    only allow authenticated users to see products

--- product/views.py
+++ product/views.py
@@ -6,12 +6,17 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 
 
 @api_view(['GET', 'POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 def products(request):
     if request.method == 'GET':
         search_str = request.GET.get('search')
