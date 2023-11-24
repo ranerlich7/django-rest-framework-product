8f63ea9160c5cbeaab22c13ece3e917df42bbfc2
Ran Erlich <Ran.Erlich@bluevine.com>
2023-11-24 06:59:36 +0200

    added github pages to cors

--- product_proj/settings.py
+++ product_proj/settings.py
@@ -47,9 +47,10 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5501",
+    "http://127.0.0.1:5500",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "https://ranerlich7.github.io",
     
 ]
 
